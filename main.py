3d900f9372bb10dcfd204c1668c3505d58ec4ba9
Phreakester <getty58@me.com>
2020-11-02 15:18:13 -0800

    Updated the qubit object to maake the approach more mathematical. Now, applying a gate is through a method which multiplies the vector and the gate, then updates the alpha, beta, and spin off of the result.

--- main.py
+++ main.py
@@ -5,51 +5,36 @@ import q_op as qop
 zero_vector = np.array([1,0])
 one_vector = np.array([0,1])
 
-operations = {
-    'x_gate' : qop.x_gate,
-    'h_gate' : qop.h_gate,
-    'set_superposition' : qop.set_superposition,
-    'measure' : qop.measure
-}
+x_gate = np.array([[0,1],[1,0]])
+h_gate = np.multiply(1/(2**0.5), np.array([[1,1],[1,-1]]))
 
 class Qubit:
     """Keeps track of all data for each qubit"""
-    def __init__(self, initial = 0):
-        self.spin = initial
-        self.alpha = None
-        self.beta = None
-
-    def set_spin(self, spin):
-        """Sets qubit spin when given a spin"""
-        if spin in [0,1]:
-            self.spin = spin
-        else:
-            print(f'Cannot set spin as {spin} is not a valid spin')
-
-    def set_superposition(self, alpha = 1/(2**0.5)):
-        """Sets qubit into superposition when given an alpha value (beta is determined)"""
-        self.spin = -1
+    def __init__(self, alpha = 1): #Default Initializes qubit as |0>
         self.alpha = alpha
         self.beta = (1 - alpha**2)**0.5 #By Born Rule |alpha|^2+|beta|^2=1
+        self.vector = np.array([self.alpha * zero_vector]) + np.array([self.beta * one_vector]) #|u> = a|0> + B|1>
+        self.spin = None #Qubit not measured yet
+
+    def apply_gate(self, gate):
+        """Applies given gate to qubit, updates and returns self.vector"""
+        self.vector = np.dot(self.vector, gate)
+        self.update_from_vector()
+        return self.vector
 
     def measure_spin(self):
         """Returns qubit spin, and determines spin if in superposition, also sets self.spin to result"""
-        if self.spin == -1:
-            if random.random() < self.alpha**2:
-                self.spin = 0
-            else:
-                self.spin = 1
+        if random.random() < self.alpha**2:
+            self.spin = 0
+        else:
+            self.spin = 1
         return self.spin
 
-    def generate_vector(self):
-        """Generates and returns qubit's vectors"""
-        if self.spin == 1:
-            vector = one_vector #0 vector |0>
-        elif self.spin == 0:
-            vector = zero_vector #1 vector |1>
-        elif self.spin == -1:
-            vector = np.array([self.alpha * zero_vector, self.beta * one_vector]) #|u> = a|0> + B|1>
-        return vector
+    def update_from_vector(self):
+        """After vector is changed ex: applied gate usew this to update alpha, beta, and spin"""
+        self.alpha = self.vector[0,0]
+        self.beta = (1 - self.alpha**2)**0.5 #By Born Rule |alpha|^2+|beta|^2=1
+        self.spin = None
 
 total_one = 0
 total_zero = 0
@@ -79,6 +64,18 @@ total_zero = 0
 # print(total_one)
 # print(total_zero)
 
-qubit1 = Qubit()
-print(qop.h_gate(qubit1))
-print(qubit1.spin)
+qubits = {}
+for num in range(0,1024):
+    qubits[num] = Qubit()
+for qubit in qubits.values():
+    qubit.apply_gate(h_gate)
+    print(qubit.vector)
+    qubit.measure_spin()
+    # print(qubit.measure_spin())
+    if qubit.spin == 1:
+        total_one += 1
+    else:
+        total_zero += 1
+
+print(total_one)
+print(total_zero)
